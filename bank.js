e07ae6fff093a606d35fcb18114ca85ecec01d9f
Mahbub Noyon <mahbubnoyon506@gmail.com>
2022-02-11 03:31:08 +0600

    First Commit

---
+++ bank.js
@@ -0,0 +1,12 @@
+
+document.getElementById('button-field').addEventListener('click', function(){
+    const emailInput = document.getElementById('email-field');
+    const showInput = emailInput.value;
+
+
+})
+
+document.getElementById('password-field').addEventListener('click', function(){
+    const passwordInput = document.getElementById('password-field');
+    const showPassword = passwordInput.value;
+})    
